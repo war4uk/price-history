fd1aa9b6f9a69bf3bb1046063a2233af01265c3c
war4uk <war4uk@gmail.com>
2016-02-01 11:45:09 +0300

    log proxy dead, pick a random proxy each time

--- modules/crawlers/proxy.manager.ts
+++ modules/crawlers/proxy.manager.ts
@@ -1,3 +1,5 @@
+import logger from "../logger";
+
 export interface IProxy {
     url: string;
     type: string;
@@ -18,7 +20,7 @@ export interface IProxyManager {
 
 class ProxyManagerImpl implements IProxyManager {
     public connectionErrorsLimit: number = 5;
-    
+
     private proxyList: IProxy[] = [
         {
             url: "217.20.83.130:3128",
@@ -27,6 +29,8 @@ class ProxyManagerImpl implements IProxyManager {
         }];
 
     public getProxy = (): IProxy => {
+        this.shuffleProxies(); // pick a random available proxy in a list
+        
         for (let i = 0; i < this.proxyList.length; i++) {
             let proxy: IProxy = this.proxyList[i];
             if (proxy.errorsOccured < this.connectionErrorsLimit) {
@@ -40,27 +44,31 @@ class ProxyManagerImpl implements IProxyManager {
         if (!url) {
             return;
         }
-        
+
         let proxy = this.getProxyByUrl(url);
-        
+
         if (proxy && proxy.errorsOccured > 0) {
             proxy.errorsOccured--;
         }
     };
-    
+
     public reportConnectionError = (url: string): void => {
         if (!url) {
             return;
         }
-        
+
         let proxy = this.getProxyByUrl(url);
-        
+
         if (proxy) {
             proxy.errorsOccured++;
+
+            if (proxy.errorsOccured === this.connectionErrorsLimit) {
+                logger.log("info", "PROXY DEAD" + " " + url + " considered dead as max consequent error number reached");
+            }
         }
     };
 
-    private getProxyByUrl = (url: string): IProxy =>{
+    private getProxyByUrl = (url: string): IProxy => {
         for (let i = 0; i < this.proxyList.length; i++) {
             let proxy: IProxy = this.proxyList[i];
 
@@ -70,6 +78,15 @@ class ProxyManagerImpl implements IProxyManager {
         }
         return null;
     };
+
+    private shuffleProxies() {
+        for (let i = this.proxyList.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = this.proxyList[i];
+            this.proxyList[i] = this.proxyList[j];
+            this.proxyList[j] = temp;
+        }
+    }
 }
 
 export var ProxyManager: IProxyManager = new ProxyManagerImpl();
